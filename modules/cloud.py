910592ac464a11a8074442f26501bc1af767b4c9
F4stZ4p <None>
2018-11-17 22:05:27 +0200

    Update cloud.py

--- modules/cloud.py
+++ modules/cloud.py
@@ -1,17 +1,21 @@
 import discord
+from io import BytesIO
 from discord.ext import commands
 
 class Cloud():
     """Naoko Cloud. Upload anything you want except NSFW content"""
     def __init__(self, bot):
         self.bot = bot
-        self.uploads_destination = self.bot.get_channel(513440818439389214)
+        self.server = "62.210.7.8:6980"
         self.thumbnail = "https://i.imgur.com/umEfgUN.png"
 
     @commands.command()
     async def upload(self, ctx):
-        b = await self.uploads_destination.send(content=f"Upload by: **{ctx.author}** | ID: **{ctx.author.id}**", file=discord.File(ctx.message.attachments[0]))
-        await ctx.send(b.attachments[0].url)
+        file = await ctx.message.attachments[0].save()
+        file = BytesIO(file).read()
+        async with ctx.typing():
+            async with self.bot.session.post(f"{self.server}/up", data={"file": file}) as r:
+                await ctx.send((await r.text()))
 
 def setup(bot):
     bot.add_cog(Cloud(bot))
