4d3a784805d124d1a7c3953e77c27d24ed95cf73
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-11-22 18:11:24 +0200

    Update image.py

--- modules/image.py
+++ modules/image.py
@@ -52,8 +52,8 @@ class ImageModule:
     def do_drake_meme(drake_bytes: bytes, no: str, yes: str, path):
         with Image.open(BytesIO(drake_bytes)) as image:
             draw = ImageDraw.Draw(image)
-            draw.text((250, 20), fill(no if len(no) < 1 else no[:1], 15), (0, 0, 0), font=ImageFont.truetype(path, 40))
-            draw.text((250, 250), fill(yes if len(yes) < 1 else yes[:1], 15), (0, 0, 0), font=ImageFont.truetype(path, 40))
+            draw.text((250, 20), fill(no if len(no) < 50 else no[:50], 15), (0, 0, 0), font=ImageFont.truetype(path, 40))
+            draw.text((250, 250), fill(yes if len(yes) < 50 else yes[:50], 15), (0, 0, 0), font=ImageFont.truetype(path, 40))
             bytes_io = BytesIO()
             image.save(bytes_io, "png")
             bytes_io.seek(0)
