d91f508d9f7726d2b148637bc94604deecf213cb
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-11-23 22:59:34 +0200

    NSFW commands aren't fun tbh (oof x2)

--- modules/fun.py
+++ modules/fun.py
@@ -162,48 +162,6 @@ class Fun():
             emdexs.set_footer(text='API is unavailable now. Try again later!', icon_url='https://raw.githubusercontent.com/F4stZ4p/resources-for-discord-bot/master/error.ico')
             await ctx.send(embed=emdexs)
 
-
-
-    @commands.command(pass_context=True, aliases=["urb","ud","urban"])
-    @commands.cooldown(1.0, 10.0, commands.BucketType.user)
-    async def urbandictionary(self, ctx, *, urbanword):
-        """
-        Looks up for word in Urban Dictionary
-        For example, @Naoko ud cat will give you definition and more of word 'cat'
-        """
-        try:
-            async with self.session.get(f"http://api.urbandictionary.com/v0/define?term={urbanword}") as resp:
-                data = await resp.json()
-                try:
-                    data["list"][0].get("word")
-                except:
-                    embederr = discord.Embed(color=0x1f2439)
-                    embederr.set_footer(text=f"No results found for {urbanword}.", icon_url=ctx.author.avatar_url)
-                    await ctx.send(embed=embederr)
-
-                else:
-                    async with self.session.get(f"http://api.urbandictionary.com/v0/define?term={urbanword}") as resp:
-                        data = await resp.json()
-                        definition = data["list"][0].get("definition")
-                        example = data["list"][0].get("example")
-                        if definition == "": definition = "No definition(s)."
-                        elif example == "": example = "No example(s)."
-                        try:
-                            if len(definition) >= 2000:
-                                raise Exception("The definition is too long.")
-                            else:
-                                await NaokoPaginator(colour=0x1f2439, title=f'Urban Dictionary: {data["list"][0].get("word")}', entries=[f'**:baby_bottle: Definition**\n```fix\n{definition}```',f'**:link: Permalink**\n**[➤ Click Me!]({data["list"][0].get("permalink")})**', f':notebook_with_decorative_cover: Word by **{data["list"][0].get("author")}**\n:pen_ballpoint: Written on **{data["list"][0].get("written_on")}**',f':thumbsup: **{data["list"][0].get("thumbs_up")}**\n:thumbsdown: **{data["list"][0].get("thumbs_down")}**'], length=1).paginate(ctx)
-                        except Exception:
-                            emberrr = discord.Embed(color=0x1f2439)
-                            emberrr.add_field(name="Link to UrbanDictionary:", value=f'**[➤ Definition - {data["list"][0].get("word")}]({data["list"][0].get("permalink")})**', inline=True)
-                            emberrr.set_footer(text='Error while sending message. Maybe, the definition is too long.', icon_url=ctx.author.avatar_url)
-                            await ctx.send(embed=emberrr)
-        except:
-            emdexs = discord.Embed(color=0x1f2439)
-            emdexs.set_footer(text='API is unavailable now. Try again later!', icon_url='https://raw.githubusercontent.com/F4stZ4p/resources-for-discord-bot/master/error.ico')
-            await ctx.send(embed=emdexs)
-
-
     @commands.command(pass_context=True, aliases=["w"])
     @commands.cooldown(1.0, 10.0, commands.BucketType.user)
     async def weather(self, ctx, *, location):
