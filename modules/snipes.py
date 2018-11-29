85b42c8b8ce7671fe1e76d5d52a70d90cc74161e
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-11-29 18:07:43 +0200

    Update snipes.py

--- modules/snipes.py
+++ modules/snipes.py
@@ -10,25 +10,36 @@ class SnipeHistory(deque):
         return "Naoko Snipe History"
 
 class Snipes():
+    """Snipe anything deleted"""
     def __init__(self, bot):
         self.bot = bot
         self.snipes = {}
         self.thumbnail = self.bot.user.avatar_url
         
+        self.bot.loop.create_task(self.cleanup())
+        
+    async def cleanup(self):
+        """Background loop to clean up snipe cache"""
+        
+        await asyncio.sleep(600) # snipes will be stored for 10 minutes
+        self.snipes = {}
+    
     async def on_message_delete(self, message):
+        """Event is triggered when message is deleted"""
         if message.channel.is_nsfw():
             return
         
         try:
             self.snipes[message.channel.id].appendleft(message)
         except:
-            self.snipes[message.channel.id] = SnipeHistory()
-            self.snipes[message.channel.id].appendleft(message)
+            self.snipes[message.channel.id] = SnipeHistory().appendleft(message)
         
     @commands.command()
  #   @commands.cooldown(1.0, 5.0, commands.BucketType.user)
-    async def snipe(self, ctx, channel: discord.TextChannel = None, index: int = 0):
+    async def snipe(self, ctx, channel: discord.TextChannel = None, index: int = None):
+        
         channel = channel or ctx.channel
+        index = index-1 or 0
         
         if channel.is_nsfw():
             await ctx.send('Attempting to snipe a NSFW channel')
@@ -36,7 +47,11 @@ class Snipes():
         else:
             sniped = self.snipes[channel.id][index]
             
-            await ctx.send(f'Message sniped by {sniped.author}\n\n{sniped.clean_content}')
+            await ctx.send(embed=discord.Embed(color=ctx.author.top_role.colour, title=f"@{sniped.author} said in #{sniped.channel}", description=sniped.clean_content))
+            
+    @snipe.error
+    async def snipe_error(self, error, ctx):
+        await ctx.send(':warning: | **No things to snipe in this channel or your index is invalid and must be in range 1-5**', delete_after=10)       
             
 def setup(bot):
     bot.add_cog(Snipes(bot))
